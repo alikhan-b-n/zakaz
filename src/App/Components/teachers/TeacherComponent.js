0071e61c26049e4dfb6718f6742ce4f1f01888ad
MustafinaDilnaz <must.dilnaz@gmail.com>
2023-09-21 02:03:41 +0600

    courses success

--- src/App/Components/teachers/TeacherComponent.js
+++ src/App/Components/teachers/TeacherComponent.js
@@ -6,18 +6,15 @@ export function TeacherComponent(prop) {
     return (
         <div className="grid 3xl:grid-cols-5 lg:grid-cols-1 flex 3xl:w-[1420] mx-auto bg-white mb-[40px] rounded font-medium">
             <div className="col-span-1 3xl:border-r-4 border-r-[#E2E9EF] lg:border-r-0 lg:border-b-4 px-[20px] pt-[15px] flex lg:flex-row
-            3xl:flex-col text-center items-center">
+            3xl:flex-col text-center items-center justify-center">
                 <img src={imgUrl}
                      alt="Teacher"
                      className="object-contain w-[197px] h-[197px] lg:w-[150px] lg:h-[150px] lg:mb-[10px] rounded-[197px] pb-[20px]"/>
-                <div className="lg:ml-[20px]">
-                    <h1 className="text-[25px] text-black 3xl:hidden lg:inline">{prop.teacher.surname} {prop.teacher.name} {prop.teacher.patronymic}</h1>
-                </div>
             </div>
             <div className="col-span-3 bg-white p-[30px]">
-                <h1 className="text-[25px] font-bold">{prop.teacher.surname} {prop.teacher.name} {prop.teacher.patronymic}</h1>
-                <p className="text-[18px] underline underline-offset-1 text-orange-500">{prop.teacher.email}</p>
-                <p className="text-[18px]">{prop.teacher.description}</p>
+                <h1 className="3xl:text-[25px] sm:text-[22px] font-[600]">{prop.teacher.surname} {prop.teacher.name} {prop.teacher.patronymic}</h1>
+                <p className="3xl:text-[18px] sm:text-[15px] underline underline-offset-1 text-orange-500">{prop.teacher.email}</p>
+                <p className="3xl:text-[18px] sm:text-[15px]">{prop.teacher.description}</p>
             </div>
         </div>
     );
