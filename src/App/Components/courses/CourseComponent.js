cc033e939b355e3f6b326d33d84b1a354939332e
MustafinaDilnaz <must.dilnaz@gmail.com>
2023-09-21 00:53:17 +0600

    courses success

--- src/App/Components/courses/CourseComponent.js
+++ src/App/Components/courses/CourseComponent.js
@@ -7,7 +7,8 @@ export const CourseComponent = (prop) => {
     return (
         <div className="3xl:mr-[20px] mb-[20px] lg:mr-0 3xl:w-[400px] 3xl:h-[330px] 2xl:w-[320px] xl:w-[250px] xl:h-[300px] lg:h-[250px] lg:w-[500px]
              md:w-[450px] sm:w-[330px] xsm:w-[250px] font-medium
-             bg-white rounded-2xl flex px-7 justify-between bg-no-repeat bg-cover" style={divStyle}>
+             bg-white rounded-2xl flex px-7 justify-between bg-no-repeat bg-cover " style={divStyle}>
+
             <div className="flex flex-col 3xl:items-baseline ">
                 <div className="3xl:h-[45px] xl:h-[70px] px-3 bg-white rounded-xl text-[16px] mt-[23px]">
                     <div className="flex justify-center items-center h-full">
@@ -22,7 +23,9 @@ export const CourseComponent = (prop) => {
                         <div className="text-[18px]">{prop.course.price} тг</div>
                     </div>
                 </div>
-                <buttin className="mt-[10px] bg-">Записаться</buttin>
+                <div className="flex justify-center">
+                    <buttin className="mt-[10px] bg-orange-500 rounded-xl p-[7px]">Записаться</buttin>
+                </div>
             </div>
             {/*<div className="flex 3xl:min-w-[270px] 3xl:max-h-[330px]*/}
             {/*xl:min-w-[250px] xl:max-h-[290px] sm:hidden bg-contain bg-no-repeat" style={divStyle}></div>*/}
--- src/App/Pages/Main.page.js
+++ src/App/Pages/Main.page.js
@@ -15,11 +15,9 @@ export function MainPage() {
                     <SidebarComponent page="main"/>
                 </div>
                 <div className="flex flex-col  3xl:w-[1420px] 2xl:w-[1100px] xl:w-[900px] lg:w-[600px] md:w-[500px] m-auto
-                                3xl:font-bold xl:font-semibold md:font-medium">
+                               font-medium">
                     <Banner/>
                     <div className="flex flex-col justify-center">
-                        <h2 className="inline-block mt-[55px] 3xl:text-[60px] 2xl:text-[55px] xl:text-[50px] lg:text-[45px] md:text-[40px] sm:text-[35px] xsm:text-[30px]
-                    m-auto">Наши курсы</h2>
                         <div className="flex m-auto lg:flex-col mt-3">
                             <OurCourses/>
                         </div>
