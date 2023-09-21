54903cfc91b135da3363ec2ec17f5b564a525fe6
MustafinaDilnaz <must.dilnaz@gmail.com>
2023-09-21 16:09:31 +0600

    courses success

--- src/App/Components/courses/CourseComponent.js
+++ src/App/Components/courses/CourseComponent.js
@@ -1,9 +1,31 @@
 import React from "react";
+import {useMutation, useQuery, useQueryClient} from 'react-query';
+import axios from 'axios';
+import {useAuthHeader} from "react-auth-kit";
+import {useAuthUser} from "react-auth-kit";
+
 
 export const CourseComponent = (prop) => {
     const divStyle = {
         'backgroundImage': `url(${'http://193.70.125.178:4000/static/' + prop.course.image })`
     }
+    const getUser = useAuthUser();
+    const user = getUser();
+    const authHeader = useAuthHeader();
+    const url = `http://193.70.125.178:4000/auth/attendCourse/${prop.course.id}`
+    const {mutate, isLoading, isError, error} = useMutation(async () =>
+        await axios.post(url, {
+            }, {
+                headers: {
+                    'Authorization': `${authHeader()}`
+                }
+            }
+        ), {
+        onSuccess: (successData) => {
+
+        }
+    })
+
     return (
         <div className="3xl:mr-[20px] mb-[20px] lg:mr-0 3xl:w-[400px] 3xl:h-[350px] 2xl:w-[320px] xl:w-[250px] xl:h-[370px] lg:h-[250px] lg:w-[500px]
              md:w-[450px] sm:w-[330px] xsm:w-[250px] sm:h-[290px] xxsm:h-[350px] font-medium
@@ -25,7 +47,7 @@ export const CourseComponent = (prop) => {
                 </div>
             </div>
             <div className="flex justify-center w-full">
-                <buttin className="mt-[10px] bg-orange-500 rounded-xl p-[7px]" >Записаться</buttin>
+                <button onClick={() => mutate({})} className="mt-[15px] bg-orange-500 text-white rounded-xl p-[10px]" >Записаться</button>
             </div>
             {/*<div className="flex 3xl:min-w-[270px] 3xl:max-h-[330px]*/}
             {/*xl:min-w-[250px] xl:max-h-[290px] sm:hidden bg-contain bg-no-repeat" style={divStyle}></div>*/}
